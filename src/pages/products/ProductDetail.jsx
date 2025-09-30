c0ac019c95f31815d2b7e7891a76b4960c78200d
S.Mariselvam <113350913+trendingvillain@users.noreply.github.com>
2025-09-30 08:17:35 +0530

    Update ProductDetail.jsx

--- src/pages/products/ProductDetail.jsx
+++ src/pages/products/ProductDetail.jsx
@@ -39,7 +39,8 @@ import { Package } from 'lucide-react';
 
 ChartJS.register(Title, Tooltip, Legend, CategoryScale, LinearScale, PointElement, LineElement);
 
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 
 const ProductDetail = () => {
   const { id } = useParams();
